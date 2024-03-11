a0306cf4b66498270215cc9aa1f49cf6550ad78e
Jaime Pineda <jpin730@pm.me>
2024-03-11 15:07:31 -0600

    feat: :tada: initial commit

---
+++ src/main/java/com/example/javaspringmarket/JavaSpringMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringmarket/JavaSpringMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
