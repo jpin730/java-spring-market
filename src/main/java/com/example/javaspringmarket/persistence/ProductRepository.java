07b4a8da44bed3ab359711852c1ceabcf81de7b4
Jaime Pineda <jpin730@pm.me>
2024-03-12 18:05:42 -0600

    refactor: :recycle: implement product repository interface

--- src/main/java/com/example/javaspringmarket/persistence/ProductRepository.java
+++ src/main/java/com/example/javaspringmarket/persistence/ProductRepository.java
@@ -1,36 +1,51 @@
 package com.example.javaspringmarket.persistence;
 
+import com.example.javaspringmarket.domain.Product;
+import com.example.javaspringmarket.domain.repository.ProductRepositoryInterface;
 import com.example.javaspringmarket.persistence.crud.ProductCrudRepository;
-import com.example.javaspringmarket.persistence.entity.Product;
+import com.example.javaspringmarket.persistence.entity.ProductEntity;
+import com.example.javaspringmarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductRepositoryInterface {
     private ProductCrudRepository productCrudRepository;
+    private ProductMapper productMapper;
 
+    @Override
     public List<Product> getAll() {
-        return (List<Product>) productCrudRepository.findAll();
+        List<ProductEntity> productEntities = (List<ProductEntity>) productCrudRepository.findAll();
+        return productMapper.toProducts(productEntities);
     }
 
+    @Override
     public Optional<Product> getById(int id) {
-        return productCrudRepository.findById(id);
+        Optional<ProductEntity> productEntity = productCrudRepository.findById(id);
+        return productEntity.map(product -> productMapper.toProduct(product));
     }
 
-    public List<Product> getByCategory(int categoryId) {
-        return productCrudRepository.findByCategoryIdOrderByNameAsc(categoryId);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<ProductEntity> productEntities = productCrudRepository.findByCategoryIdOrderByNameAsc(categoryId);
+        return Optional.of(productMapper.toProducts(productEntities));
     }
 
+    @Override
     public Optional<List<Product>> getScarceProducts(int quantity) {
-        return productCrudRepository.findByStockLessThanAndStatus(quantity, true);
+        Optional<List<ProductEntity>> productEntities = productCrudRepository.findByStockLessThanAndStatus(quantity, true);
+        return productEntities.map(product -> productMapper.toProducts(product));
     }
 
+    @Override
     public Product save(Product product) {
-        return productCrudRepository.save(product);
+        ProductEntity productEntity = productMapper.toProductEntity(product);
+        return productMapper.toProduct(productCrudRepository.save(productEntity));
     }
 
+    @Override
     public void delete(int id) {
         productCrudRepository.deleteById(id);
     }
