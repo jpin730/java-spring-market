fbc442c931809a1e03494a6d87fc6c0ccd9ecd5e
Jaime Pineda <jpin730@pm.me>
2024-03-12 18:03:20 -0600

    feat: :sparkles: add category and product class for domain

---
+++ src/main/java/com/example/javaspringmarket/domain/Category.java
@@ -0,0 +1,31 @@
+package com.example.javaspringmarket.domain;
+
+public class Category {
+    private int id;
+    private String name;
+    private boolean active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
